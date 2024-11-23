5704757e3c1000ed4129f2bf7bf4bde31a7fbaf6
SafouaneN <98953182+SafouaneN@users.noreply.github.com>
2024-11-23 15:08:55 +0100

    Delete MS1 directory

--- MS1/requirements.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-pip install "pandas" "torchvision" "torch>=1.8" "torchmetrics>=0.7" "seaborn" "pytorch-lightning>=1.4" "numpy" "albumentations" "tensorflow"
-mkdir evaluation # TODO in python
